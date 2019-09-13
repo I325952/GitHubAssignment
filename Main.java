57392fd19cc3b6f4234b8eed2eae53a4774f50ba
RitikaMehrotra <ritika.16sep@gmail.com>
2019-09-13 11:19:25 +0530

    By C

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("Tiktok_TestAutomation_script_V1");
                 System.out.println("Tiktok_TestAutomation_script_V2");
                 System.out.println("Tiktok_TestAutomation_script_V3");
+                System.out.println("Tiktok_TestAutomation_script_RitikaV4");
                
 	}
         
